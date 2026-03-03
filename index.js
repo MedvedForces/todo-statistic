40a5d8bd5ff9eb23c5a2163ca217660eb7f5e6de
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2026-03-03 17:54:50 +0500

    task 1 complete

--- index.js
+++ index.js
@@ -11,10 +11,39 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getComment() {
+    const todoComments = [];
+    
+    files.forEach(fileContent => {
+        const lines = fileContent.split('\n');
+        
+        lines.forEach((line, index) => {
+            const commentStart = line.indexOf('//');
+            
+            if (commentStart !== -1 && line.includes('TODO')) {
+                const commentText = line.substring(commentStart).trim();
+                todoComments.push({
+                    line: index + 1,
+                    text: commentText
+                });
+            }
+        });
+    });
+    
+    return todoComments;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            const arrayComment = getComment();
+            for (const com of arrayComment) {
+                console.log(com.text);
+            }
+
             break;
         default:
             console.log('wrong command');
@@ -23,3 +52,5 @@ function processCommand(command) {
 }
 
 // TODO you can do it!
+
+
